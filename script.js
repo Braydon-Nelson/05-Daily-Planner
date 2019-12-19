145ac11cb1a3a8d798eb81bb2a6dfdee275846d6
Braydon Nelson <braydon.nelson@outlook.com>
2019-12-18 23:35:53 -0700

    FINISHED!!! made local storage work both setting it and getting it when the page reloads!

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 $(document).ready(function () {
-    $(".saveBtn").on("click", function () {
-        console.log($(this));
 
+    // Place items in text areas from local storage for each text area
+    $("textarea").each(function () {
+        var item = localStorage.getItem("area" + $(this).data("field").toString());
+        console.log(item);
+        $(this).val(item);
+    });
+
+    // Save items to loacl storage
+    $(".saveBtn").on("click", function () {
+        // Select item before button
+        var text = $(this).prev().val()
+        // Select area's data number
+        var textArea = $(this).prev().data("field")
+        // Set item in local storage
+        localStorage.setItem("area" + textArea, text);
+        console.log("Text Area " + textArea + " saved!")
     })
 
-
+    // keep checking the time and update the page
     function hoursUpdater() {
-        var currentDay = moment().day();
+        // Instantly places day of the week into #currentDay
         $("#currentDay").text(moment().format('dddd'));
 
+        // Check current hour
         var currentHour = moment().hours();
 
         $(".time-block").each(function () {
-
+            // see what the block's hour is
             var blockHour = parseInt($(this).attr("id").split("-")[1]);
-
+            // compare current our to block's hour and color accordingly
             if (blockHour < currentHour) {
                 $(this.children[1]).addClass("past");
             } else if (blockHour > currentHour) {
@@ -25,11 +40,12 @@ $(document).ready(function () {
 
         })
 
-        console.log("Function did run");
+        console.log("Function ran");
     }
 
     hoursUpdater();
 
+    // check time every 10 seconds
     var interval = setInterval(hoursUpdater, 10000)
 
 });
